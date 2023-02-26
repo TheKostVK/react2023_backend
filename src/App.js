6f9bb7465723a02d9a50ac92f63f9684fc993ea1
TheKost_ <thekost2@yandex.ru>
2023-02-26 23:55:21 +0300

    v1.0.2(6)

--- src/App.js
+++ src/App.js
@@ -1,41 +1,13 @@
 import './App.css';
 import './mycss.css';
-import { Card } from 'antd';
-import React, { useState } from 'react';
+import {Card} from 'antd';
+import React, {useState} from 'react';
 
 function App() {
     // Состояния для списка пользователей, постов пользователя и кнопки
     const [users, setUsers] = useState([]);
     const [userPosts, setUserPosts] = useState([]);
     const [usersButtonLabel, setUsersButtonLabel] = useState('Получить список пользователей');
-
-    // Функция для получения списка пользователей
-    const getUsers = async () => {
-        try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/users');
-            const data = await response.json();
-            // Если данные получены, сохраняем их в состояние
-            if (Array.isArray(data) && data.length > 0) {
-                setUsers(data);
-            }
-        } catch (error) {
-            console.error('Ошибка получения users', error);
-        }
-    };
-
-    // Функция для получения постов пользователей
-    const getUserPosts = async () => {
-        try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-            const data = await response.json();
-            // Если данные получены, сохраняем их в состояние
-            if (Array.isArray(data) && data.length > 0) {
-                setUserPosts(data);
-            }
-        } catch (error) {
-            console.error('Ошибка получения user posts', error);
-        }
-    };
 
     // Функция для получения постов пользователей и списка пользователей
     const getUsersAndPosts = async () => {
@@ -64,8 +36,7 @@ function App() {
     // Функция, которая вызывается при нажатии на кнопку "Получить список пользователей"
     const handleGetUsers = () => {
         // Получаем список пользователей и их постов
-        getUsers();
-        getUserPosts();
+        getUsersAndPosts()
         // Обновляем надпись на кнопке
         setUsersButtonLabel('Обновить список пользователей');
     };
@@ -90,37 +61,37 @@ function App() {
                 {users.map((user) => {
                     const userPostList = userPosts.filter((post) => post.userId === user.id);
                     return (
-                        <Card style={{ margin: 15 }} title={user.name} key={user.id}>
+                        <Card style={{margin: 15}} title={user.name} key={user.id}>
                             <p>
                                 <strong>Email: </strong>
-                                <strong style={{ color: 'red' }}>{user.email}</strong>
+                                <strong style={{color: 'red'}}>{user.email}</strong>
                             </p>
                             <p>
                                 <strong>Номер телефона: </strong>
-                                <strong style={{ color: '#6A5ACD' }}>{user.phone}</strong>
+                                <strong style={{color: '#6A5ACD'}}>{user.phone}</strong>
                             </p>
                             <p>
                                 <strong>Веб-сайт: </strong>
                                 <a href={user.website}>{user.website}</a>
                             </p>
-                            <div style={{ border: '1px solid #000', borderRadius: 2 }}>
+                            <div style={{border: '1px solid #000', borderRadius: 2}}>
                                 <details>
-                                    <summary style={{ fontWeight: 'bold', marginBottom: 10 }}>
+                                    <summary style={{fontWeight: 'bold', marginBottom: 10}}>
                                         Посты пользователя
                                     </summary>
                                     {userPostList.length > 0 ? (
                                         userPostList.map((post) => (
                                             <Card
                                                 title={post.title}
-                                                style={{ margin: 10 }}
-                                                headStyle={{ background: 'black', color: '#fff' }}
+                                                style={{margin: 10}}
+                                                headStyle={{background: 'black', color: '#fff'}}
                                                 key={post.id}
                                             >
                                                 <p>{post.body}</p>
                                             </Card>
                                         ))
                                     ) : (
-                                        <p style={{ margin: 10 }}>Постов нет</p>
+                                        <p style={{margin: 10}}>Постов нет</p>
                                     )}
                                 </details>
                             </div>
