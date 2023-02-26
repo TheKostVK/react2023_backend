f68205b15b4da29fa670df070c8d90a1f8691644
TheKost_ <thekost2@yandex.ru>
2023-02-26 23:32:26 +0300

    v1.0.2(4)

--- src/App.js
+++ src/App.js
@@ -1,105 +1,109 @@
 import './App.css';
 import './mycss.css';
-import {Card, Row} from 'antd'
-import React, {useState} from "react";
+import { Card, Row } from 'antd';
+import React, { useState, useEffect } from 'react';
 
+function App() {
+    // Состояния для списка пользователей, постов пользователя и кнопки
+    const [users, setUsers] = useState([]);
+    const [userPosts, setUserPosts] = useState([]);
+    const [usersButtonLabel, setUsersButtonLabel] = useState('Получить список пользователей');
 
-// Функциональный компонент - четкий, модный, молодежный
-function App1() {
-    const [users, setUsers] = useState([])
-    const [userPosts, setPosts] = useState([])
-    const [usersButtonTEXT, setUsersButtonTEXT] = useState(['Получить список пользователей'])
-
-    // Обновляет текст на кнопке получения списка пользователей
-    const updateButtonTEXT = (updateTEXT) => {
-        setUsersButtonTEXT(updateTEXT)
-    }
-    // Получение данных по ссылке
-    const getData = () => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            // Конвертация в json файл
-            .then(res => res.json())
-            // Проверка на массив
-            .then(res => {
-                if (res && Array.isArray(res) && res.length > 0) {
-                    setUsers(res)
-                }
-            })
-        // Получение постов
-        fetch('https://jsonplaceholder.typicode.com/posts').then(Posts => Posts.json()).then(Posts => {
-            if (Posts && Array.isArray(Posts) && Posts.length > 0) {
-                setPosts(Posts);
-                console.log(Posts)
+    // Функция для получения списка пользователей
+    const getUsers = async () => {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+            const data = await response.json();
+            // Если данные получены, сохраняем их в состояние
+            if (Array.isArray(data) && data.length > 0) {
+                setUsers(data);
             }
-        })
-        // Обновление текста на кнопке
-        updateButtonTEXT('Обновить список пользователей');
-    }
-    const stylesCSS = {
-        color: 'red',
-        border: '1px solid #000',
-        padding: 10,
-        margin: 'auto',
-        borderRadius: 10,
-        marginBottom: 5,
-        background: '#ccc'
-    }
-    const styleDefault = {
-        margin: 15
-    }
+        } catch (error) {
+            console.error('Ошибка получения users', error);
+        }
+    };
+
+    // Функция для получения постов пользователей
+    const getUserPosts = async () => {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+            const data = await response.json();
+            // Если данные получены, сохраняем их в состояние
+            if (Array.isArray(data) && data.length > 0) {
+                setUserPosts(data);
+            }
+        } catch (error) {
+            console.error('Ошибка получения user posts', error);
+        }
+    };
+
+    // Функция, которая вызывается при нажатии на кнопку "Получить список пользователей"
+    const handleGetUsers = () => {
+        // Получаем список пользователей и их постов
+        getUsers();
+        getUserPosts();
+        // Обновляем надпись на кнопке
+        setUsersButtonLabel('Обновить список пользователей');
+    };
+
+    // Функция для форматирования даты
+    const formatDate = (date) => {
+        return new Intl.DateTimeFormat('ru-RU').format(date);
+    };
+
     return (
         <>
-            <h1 style={styleDefault}>Список пользователей: </h1>
-            <button className='button-main' style={styleDefault} type=""
-                    onClick={() => getData()}>{usersButtonTEXT}</button>
-            {
-                usersButtonTEXT != 'Получить список пользователей' &&
-                <p className='subsection-performance-headline'
-                   style={styleDefault}>{'Дата последнего обновления: ' + new Date().toLocaleString() + ''}</p>
-            }
+            <h1 style={{margin: 15}}>Список пользователей:</h1>
+            <button style={{margin: 15}} className="button-main" type="button" onClick={handleGetUsers}>
+                {usersButtonLabel}
+            </button>
+            {usersButtonLabel !== 'Получить список пользователей' && (
+                <p style={{margin: 15}} className="subsection-performance-headline">
+                    {'Дата последнего обновления: ' + formatDate(new Date())}
+                </p>
+            )}
             <div>
-                {users.length > 0 && users.map(user => {
+                {users.map((user) => {
+                    const userPostList = userPosts.filter((post) => post.userId === user.id);
                     return (
-                        <Card style={{margin: 15}} title={user.name} key={Math.random()}>
+                        <Card style={{ margin: 15 }} title={user.name} key={user.id}>
                             <p>
                                 <strong>Email: </strong>
-                                <strong style={{color: 'red'}}>{user.email}</strong>
+                                <strong style={{ color: 'red' }}>{user.email}</strong>
                             </p>
                             <p>
                                 <strong>Номер телефона: </strong>
-                                <strong style={{color: '#6A5ACD'}}>{user.phone}</strong>
+                                <strong style={{ color: '#6A5ACD' }}>{user.phone}</strong>
                             </p>
                             <p>
                                 <strong>Веб-сайт: </strong>
                                 <a href={user.website}>{user.website}</a>
                             </p>
-                            <div style={{border: '1px solid #000', borderRadius: 2}}>
+                            <div style={{ border: '1px solid #000', borderRadius: 2 }}>
                                 <details>
                                     <summary>Нажмите чтобы увидеть посты пользователя</summary>
-                                    <Row style={{margin: 10}} gutter={10}>
-                                        {
-                                            userPosts.filter(post => post.userId === user.id)
-                                                .map(post => {
-                                                        return (
-                                                            <Card title={post.title} style={{margin: 10, width: "100%"}}
-                                                                  headStyle={{background: 'black', color: '#fff'}}>
-                                                                <p>{post.body}</p>
-                                                            </Card>
-                                                        )
-                                                    }
-                                                )
-                                        }
+                                    <Row style={{ margin: 10 }} gutter={10}>
+                                        {userPostList.map((post) => {
+                                            return (
+                                                <Card
+                                                    title={post.title}
+                                                    style={{ margin: 10, width: '100%' }}
+                                                    headStyle={{ background: 'black', color: '#fff' }}
+                                                    key={post.id}
+                                                >
+                                                    <p>{post.body}</p>
+                                                </Card>
+                                            );
+                                        })}
                                     </Row>
                                 </details>
                             </div>
                         </Card>
                     );
-                })
-                }
+                })}
             </div>
         </>
-    )
+    );
 }
 
-
-export default App1;
+export default App;
