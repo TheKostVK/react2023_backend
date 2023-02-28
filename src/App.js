2477653497ecef84a878cbc3cce5ca2b9bc2d69a
TheKost_ <thekost2@yandex.ru>
2023-02-28 21:57:12 +0300

    v1.0.2(6)

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import {HelloWorld, MainNavBar} from './components/';
 import {Card} from 'antd';
 import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 
 
 function App() {
+    return (
+        <MainNavBar>
+            <Routes>
+                <Route path='/' element={ <HomePage /> }/>
+            </Routes>
+            <div></div>
+        </MainNavBar>
+    );
+}
+
+
+const HomePage = () => {
     // Состояния для списка пользователей, постов пользователя и кнопки
     const [users, setUsers] = useState([]);
     const [userPosts, setUserPosts] = useState([]);
@@ -32,85 +44,84 @@ function App() {
                 setUserPosts(postsData);
             }
         } catch (error) {
-            console.error('Ошибка получения данных', error);
+            console.error('Ошибка получения данных: ', error);
         }
     };
 
 
     // Функция, которая вызывается при нажатии на кнопку "Получить список пользователей"
-    const handleGetUsers = () => {
-        // Получаем список пользователей и их постов
-        getUsersAndPosts()
-        // Обновляем надпись на кнопке
-        setUsersButtonLabel('Обновить список пользователей');
+    const handleGetUsers = async () => {
+        try {
+            // Получаем список пользователей и их постов
+            await getUsersAndPosts();
+            // Обновляем надпись на кнопке
+            setUsersButtonLabel('Обновить список пользователей');
+        } catch (error) {
+            console.error('Ошибка получения списка пользователей: ',error);
+        }
     };
+
 
     // Функция для форматирования даты
     const formatDate = (date) => {
         return new Intl.DateTimeFormat('ru-RU').format(date);
     };
-
-    return (
-        <MainNavBar>
-            <Routes>
-
-            </Routes>
+    return(
+        <div>
+            <h1 style={{margin: 15}}>Список пользователей:</h1>
+            <button style={{margin: 15}} className="button-main" type="button" onClick={handleGetUsers}>
+                {usersButtonLabel}
+            </button>
+            {usersButtonLabel !== 'Получить список пользователей' && (
+                <p style={{margin: 15}}>
+                    Дата последнего обновления: <strong
+                    className="subsection-performance-headline">{formatDate(new Date())}</strong>
+                </p>
+            )}
             <div>
-                <h1 style={{margin: 15}}>Список пользователей:</h1>
-                <button style={{margin: 15}} className="button-main" type="button" onClick={handleGetUsers}>
-                    {usersButtonLabel}
-                </button>
-                {usersButtonLabel !== 'Получить список пользователей' && (
-                    <p style={{margin: 15}}>
-                        Дата последнего обновления: <strong
-                        className="subsection-performance-headline">{formatDate(new Date())}</strong>
-                    </p>
-                )}
-                <div>
-                    {users.map((user) => {
-                        const userPostList = userPosts.filter((post) => post.userId === user.id);
-                        return (
-                            <Card style={{margin: 15}} title={user.name} key={user.id}>
-                                <p>
-                                    <strong>Email: </strong>
-                                    <strong style={{color: 'red'}}>{user.email}</strong>
-                                </p>
-                                <p>
-                                    <strong>Номер телефона: </strong>
-                                    <strong style={{color: '#6A5ACD'}}>{user.phone}</strong>
-                                </p>
-                                <p>
-                                    <strong>Веб-сайт: </strong>
-                                    <a href={user.website}>{user.website}</a>
-                                </p>
-                                <div style={{border: '1px solid #000', borderRadius: 2}}>
-                                    <details>
-                                        <summary style={{fontWeight: 'bold', marginBottom: 10}}>
-                                            Посты пользователя
-                                        </summary>
-                                        {userPostList.length > 0 ? (
-                                            userPostList.map((post) => (
-                                                <Card
-                                                    title={post.title}
-                                                    style={{margin: 10}}
-                                                    headStyle={{background: 'black', color: '#fff'}}
-                                                    key={post.id}
-                                                >
-                                                    <p>{post.body}</p>
-                                                </Card>
-                                            ))
-                                        ) : (
-                                            <p style={{margin: 10}}>Постов нет</p>
-                                        )}
-                                    </details>
-                                </div>
-                            </Card>
-                        );
-                    })}
-                </div>
+                {users.map((user) => {
+                    const userPostList = userPosts.filter((post) => post.userId === user.id);
+                    return (
+                        <Card style={{margin: 15}} title={user.name} key={user.id}>
+                            <p>
+                                <strong>Email: </strong>
+                                <strong style={{color: 'red'}}>{user.email}</strong>
+                            </p>
+                            <p>
+                                <strong>Номер телефона: </strong>
+                                <strong style={{color: '#6A5ACD'}}>{user.phone}</strong>
+                            </p>
+                            <p>
+                                <strong>Веб-сайт: </strong>
+                                <a href={user.website}>{user.website}</a>
+                            </p>
+                            <div style={{border: '1px solid #000', borderRadius: 2}}>
+                                <details>
+                                    <summary style={{fontWeight: 'bold', marginBottom: 10}}>
+                                        Посты пользователя
+                                    </summary>
+                                    {userPostList.length > 0 ? (
+                                        userPostList.map((post) => (
+                                            <Card
+                                                title={post.title}
+                                                style={{margin: 10}}
+                                                headStyle={{background: 'black', color: '#fff'}}
+                                                key={post.id}
+                                            >
+                                                <p>{post.body}</p>
+                                            </Card>
+                                        ))
+                                    ) : (
+                                        <p style={{margin: 10}}>Постов нет</p>
+                                    )}
+                                </details>
+                            </div>
+                        </Card>
+                    );
+                })}
             </div>
-        </MainNavBar>
-    );
+        </div>
+    )
 }
 
 export default App;
