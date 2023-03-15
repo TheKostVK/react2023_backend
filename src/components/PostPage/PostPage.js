60d24fbcb1b487afc3de30698aadabe8e5a9ba58
Vlad Kost <kostvkthekost@gmail.com>
2023-03-16 01:09:21 +0300

    v1.2.0

--- src/components/PostPage/PostPage.js
+++ src/components/PostPage/PostPage.js
@@ -1,60 +1,64 @@
-import React, {useEffect, useState} from "react";
-import {useParams} from "react-router-dom";
-import {Typography, Divider, Skeleton} from "antd";
-import moment from "moment/moment";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Typography, Divider, Skeleton } from "antd";
+import moment from "moment";
 
-const {Title} = Typography
-const { Paragraph } = Typography;
+const { Title, Paragraph } = Typography;
 
 export const PostPage = () => {
     const [post, setPost] = useState({});
-    const params = useParams();
     const [isLoading, setLoading] = useState(true);
-
-    const {id} = params;
+    const { id } = useParams();
 
     useEffect(() => {
-        getPosts();
-    }, []);
-
-    const getPosts = async () => {
-        try {
-            if (id.length > 0) {
-                const postResponse = await fetch(`http://localhost:3900/post/${id}`);
-                const postData = await postResponse.json();
-                if (postData.success && typeof postData.post == 'object') {
-                    setPost(postData.post)
-                    setLoading(false)
+        const getPost = async () => {
+            try {
+                if (id.length > 0) {
+                    const postResponse = await fetch(`http://localhost:3900/post/${id}`);
+                    const postData = await postResponse.json();
+                    if (postData.success && typeof postData.post === "object") {
+                        setPost(postData.post);
+                        setLoading(false);
+                    }
                 }
+            } catch (error) {
+                console.error("Ошибка получения данных: ", error);
             }
-        } catch (error) {
-            console.error('Ошибка получения данных: ', error);
-        }
-    };
+        };
+
+        getPost();
+    }, [id]);
+
     return (
-        <div style={{margin: 20, textAlign: 'center'}}>
+        <div style={{ margin: 20, textAlign: "center" }}>
             {isLoading && (
                 <>
                     <div>
-                        <Title level={1}>{<Skeleton.Input  active/>}</Title>
-                        <Divider style={{fontSize: 12}}>{<Skeleton.Input active/>}</Divider>
+                        <Title level={1}>
+                            <Skeleton.Input active />
+                        </Title>
+                        <Divider style={{ fontSize: 12 }}>
+                            <Skeleton.Input active />
+                        </Divider>
                     </div>
                     <div>
                         <Skeleton />
                     </div>
                 </>
             )}
-            {!isLoading && post && post?.title && post?.short_desc && post?.full_desc &&
+            {!isLoading && post?.title && post?.short_desc && post?.full_desc && (
                 <>
                     <div>
                         <Title level={1}>{post.title}</Title>
-                        <Divider style={{fontSize: 12}}>{moment(post.create_date).format('DD.MM.YY HH:m:ss')}</Divider>
+                        <Divider style={{ fontSize: 12 }}>
+                            {moment(post.create_date).format("DD.MM.YY HH:m:ss")}
+                        </Divider>
                     </div>
                     <div>
                         <Paragraph>{post.full_desc}</Paragraph>
                     </div>
                 </>
-            }
+            )}
         </div>
-    )
-}
+    );
+};
--- src/components/PostsPage/Posts.js
+++ src/components/PostsPage/Posts.js
@@ -1,41 +1,36 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect} from "react";
 import moment from "moment";
 import {Card, Skeleton} from 'antd';
 import {ButtonUI} from "../index";
 import {useNavigate} from "react-router-dom";
+import {useDispatch, useSelector} from "react-redux";
+import {getPosts} from "../../store/actions/postActions";
 
 export const Posts = () => {
     const navigation = useNavigate()
-    const [posts, setPosts] = useState([]);
-    const [isLoading, setLoading] = useState(true);
+    const dispatch = useDispatch();
+    const {loading, success, posts} = useSelector((state => state.posts));
 
     useEffect(() => {
-        getPosts();
-    }, []);
-
-    const getPosts = async () => {
-        try {
-            const postsResponse = await fetch('http://localhost:3900/posts');
-            const postsData = await postsResponse.json();
-
-            if (postsData.success && Array.isArray(postsData.posts) && postsData.posts.length > 0) {
-                setPosts(postsData.posts);
-                setLoading(false);
+        const fetchData = async () => {
+            try {
+                dispatch({type: 'getPosts_onfetch'});
+                const data = await getPosts();
+                dispatch({type: 'getPosts_success', posts: data.posts, loading: false, success: true});
+            } catch (error) {
+                dispatch({type: 'getPosts_failure', errMsg: error.message});
             }
-        } catch (error) {
-            console.error('Ошибка получения данных: ', error);
-        }
-    };
+        };
+        fetchData();
+    }, [dispatch]);
 
-    const goToPost = (id = '') => {
-        if (id.length > 0) {
-            navigation(`/post/${id}`)
-        }
+    const goToPost = (id) => {
+        navigation(`/post/${id}`)
     }
 
     return (
         <>
-            {isLoading && (
+            {loading ? (
                 <>
                     <Card title={<Skeleton.Input active/>} bordered={false} style={{margin: 20}}>
                         <Skeleton active/>
@@ -44,20 +39,36 @@ export const Posts = () => {
                         <Skeleton active/>
                     </Card>
                 </>
-            )}
-            {!isLoading && posts && Array.isArray(posts) && (
+            ) : (
                 <>
-                    {posts.map((item) => {
-                        return (
-                            <Card key={item._id} title={item.title} bordered={false}
-                                  style={{margin: 20}}>
-                                <p>{item.short_desc}</p>
-                                <p>{item.full_desc}</p>
-                                <p>{moment(item.create_date).format('DD.MM.YY HH:m:ss')}</p>
-                                <ButtonUI label={'Открыть'} onClick={() => goToPost(item._id)}/>
-                            </Card>
-                        );
-                    })}
+                    {posts && posts.length > 0 ? (
+                        <>
+                            {posts.map((item) => (
+                                <Card key={item._id} title={item.title} bordered={false} style={{margin: 20}}>
+                                    <p>{item.short_desc}</p>
+                                    <p>{item.full_desc}</p>
+                                    <p>{moment(item.create_date).format('DD.MM.YY HH:m:ss')}</p>
+                                    <ButtonUI label={'Открыть'} onClick={() => goToPost(item._id)}/>
+                                </Card>
+                            ))}
+                        </>
+                    ) : (
+                        <>
+                            <div>
+                                <p style={{margin: 20, color: 'red'}}>
+                                    Ошибка загрузки данных, повторите попытку позже
+                                </p>
+                                <Card key='1' title={<Skeleton.Input active/>} bordered={false}
+                                      style={{margin: 20}}>
+                                    <Skeleton active/>
+                                </Card>
+                                <Card key='2' title={<Skeleton.Input active/>} bordered={false}
+                                      style={{margin: 20}}>
+                                    <Skeleton active/>
+                                </Card>
+                            </div>
+                        </>
+                    )}
                 </>
             )}
         </>
