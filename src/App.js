2acdd0101bb64431c1f06fb607195c4c263a3034
Vlad Kost <kostvkthekost@gmail.com>
2023-03-18 01:20:18 +0300

    v1.2.0

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
 import './mycss.css';
 import {ButtonUI, MainNavBar, PostPage, Posts} from './components/';
-import {Card, Skeleton, Result} from 'antd';
-import React, {useEffect, useState} from 'react';
+import {Result, List, Avatar, Space} from 'antd';
+import React from 'react';
 import {Route, Routes, useLocation} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
-import {getUsers} from "./store/actions/usersActions";
-import {getUserPosts} from "./store/actions/userPostsActions";
+import {LikeOutlined, MessageOutlined, StarOutlined} from "@ant-design/icons";
+import Title from "antd/es/typography/Title";
 
 
 function App() {
@@ -23,138 +22,68 @@ function App() {
 }
 
 
-const HomePage = () => {
-    const [userPosts, setUserPosts] = useState([]);
-    const dispatch = useDispatch();
-    const {loading, users} = useSelector((state) => state.users);
+const data = Array.from({
+    length: 23,
+}).map((_, i) => ({
+    href: 'https://ant.design',
+    title: `ant design part ${i}`,
+    avatar: `https://joesch.moe/api/v1/random?key=${i}`,
+    description:
+        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+    content:
+        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+}));
+
+const IconText = ({icon, text}) => (
+    <Space>
+        {React.createElement(icon)}
+        {text}
+    </Space>
+);
 
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                dispatch({type: 'getUsers_onfetch'});
-                const data = await getUsers();
-                dispatch({type: 'getUsers_success', users: data, loading: false, success: true});
-            } catch (error) {
-                dispatch({type: 'getUsers_failure', errMsg: error.message});
-            }
-        };
-        fetchData();
-    }, [dispatch]);
+const HomePage = () => (
+    <>
+        <Title level={1} style={{textAlign: "center"}}>Главная</Title>
+        <List style={{margin: 20}}
+              itemLayout="vertical"
+              size="large"
+              pagination={{
+                  onChange: (page) => {
+                      console.log(page);
+                  },
+                  pageSize: 3,
+              }}
 
+              dataSource={data}
 
-    // Функция для форматирования даты
-    const formatDate = (date) => {
-        return new Intl.DateTimeFormat('ru-RU').format(date);
-    };
-
-
-    return (
-        <div>
-            <h1 style={{margin: 20}}>Список пользователей:</h1>
-            <p style={{margin: 20}}>
-                Дата последнего обновления:
-                <strong className="subsection-performance-headline">{formatDate(new Date())}</strong>
-            </p>
-            <div>
-                {loading ? (
-                    <>
-                        <Card key='1' title={<Skeleton.Input active/>} bordered={false} style={{margin: 20}}>
-                            <Skeleton active/>
-                        </Card>
-                        <Card key='2' title={<Skeleton.Input active/>} bordered={false} style={{margin: 20}}>
-                            <Skeleton active/>
-                        </Card>
-                    </>
-                ) : (
-                    <>
-                        {users && users.length > 0 ? (
-                            <>
-                                {users.map((user) => {
-                                    const userPostList = userPosts.filter((post) => post.userId === user.id);
-                                    return (
-                                        <Card style={{margin: 20}}
-                                              headStyle={{background: 'dimgrey', color: '#fff'}}
-                                              title={user.name} key={user.id}>
-                                            <p>
-                                                <strong>Email: </strong>
-                                                <strong style={{color: 'red'}}>{user.email}</strong>
-                                            </p>
-                                            <p>
-                                                <strong>Номер телефона: </strong>
-                                                <strong style={{color: '#6A5ACD'}}>{user.phone}</strong>
-                                            </p>
-                                            <p>
-                                                <strong>Веб-сайт: </strong>
-                                                <a href={user.website}>{user.website}</a>
-                                            </p>
-                                            <ButtonUI type={"primary"} label={"Загрузить посты"} className={"ant-btn-primary ant-btn"}
-                                                      style={{textDecoration: 'none'}} onClick={() => {
-                                                const fetchData = async () => {
-                                                    try {
-                                                        dispatch({type: 'getUserPosts_onfetch'});
-                                                        const data = await getUserPosts(user.id);
-                                                        setUserPosts(data);
-                                                        dispatch({
-                                                            type: 'getUserPosts_success',
-                                                            loading: false,
-                                                            success: true
-                                                        });
-                                                    } catch (error) {
-                                                        dispatch({type: 'getUserPosts_failure', errMsg: error.message});
-                                                    }
-                                                };
-                                                fetchData();
-                                            }}>
-                                                Загрузить посты
-                                            </ButtonUI>
-                                            <div style={{border: '1px solid #000', borderRadius: 2, marginTop: 15}}>
-                                                <details>
-                                                    <summary style={{fontWeight: 'bold', marginBottom: 10}}>
-                                                        Посты пользователя
-                                                    </summary>
-                                                    {userPostList.length > 0 ? (
-                                                        userPostList.map((post) => (
-                                                            <Card title={post.title} style={{margin: 10}}
-                                                                  headStyle={{
-                                                                      background: 'dimgrey',
-                                                                      color: '#fff'
-                                                                  }} key={post.id}>
-                                                                <p>{post.body}</p>
-                                                            </Card>
-                                                        ))
-                                                    ) : (
-                                                        <p style={{margin: 10}}>Постов нет</p>
-                                                    )}
-                                                </details>
-                                            </div>
-                                        </Card>
-                                    );
-                                })}
-                            </>
-                        ) : (
-                            <>
-                                <div>
-                                    <p style={{margin: 20, color: 'red'}}>
-                                        Ошибка загрузки данных, повторите попытку позже
-                                    </p>
-                                    <Card key='1' title={<Skeleton.Input active/>} bordered={false}
-                                          style={{margin: 20}}>
-                                        <Skeleton active/>
-                                    </Card>
-                                    <Card key='2' title={<Skeleton.Input active/>} bordered={false}
-                                          style={{margin: 20}}>
-                                        <Skeleton active/>
-                                    </Card>
-                                </div>
-                            </>
-                        )}
-                    </>
-                )}
-            </div>
-        </div>
-    )
-}
+              renderItem={(item) => (
+                  <List.Item
+                      style={{margin: 10, border: 'solid', backgroundColor: 'white'}}
+                      key={item.title}
+                      actions={[
+                          <IconText icon={StarOutlined} text="156" key="list-vertical-star-o"/>,
+                          <IconText icon={LikeOutlined} text="156" key="list-vertical-like-o"/>,
+                          <IconText icon={MessageOutlined} text="2" key="list-vertical-message"/>,
+                      ]}
+                      extra={
+                          <img
+                              width={272}
+                              alt="logo"
+                              src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
+                          />
+                      }>
+                      <List.Item.Meta
+                          avatar={<Avatar src={item.avatar}/>}
+                          title={<a href={item.href}>{item.title}</a>}
+                          description={item.description}
+                      />
+                      {item.content}
+                  </List.Item>
+              )}
+        />
+    </>
+);
 
 
 const NotFoundError = () => {
@@ -163,15 +92,16 @@ const NotFoundError = () => {
     const error = new Error(message);
     error.statusCode = 404;
     return (
-    <Result
-        status="404"
-        title="404"
-        subTitle={message}
-        extra={
-        <ButtonUI type={"primary"} className={"ant-btn-primary ant-btn"} to={"/"} style={{textDecoration: 'none'}}
-                         label={"Вернуться на главную"}/>
-        }
-    />
+        <Result
+            status="404"
+            title="404"
+            subTitle={message}
+            extra={
+                <ButtonUI type={"primary"} className={"ant-btn-primary ant-btn"} to={"/"}
+                          style={{textDecoration: 'none'}}
+                          label={"Вернуться на главную"}/>
+            }
+        />
     );
 };
 
