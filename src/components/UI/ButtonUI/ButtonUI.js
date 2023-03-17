ff744d6219f00a794d503e0f45a54e2af8045355
Vlad Kost <kostvkthekost@gmail.com>
2023-03-17 13:18:01 +0300

    v1.2.0

--- src/components/UI/ButtonUI/ButtonUI.js
+++ src/components/UI/ButtonUI/ButtonUI.js
@@ -1,21 +1,21 @@
 import { Button } from "antd";
 import {Link} from "react-router-dom";
 
-export const ButtonUI = ({ to, label, urlTo , style, size = 'small', type = 'primary', onClick }) => {
+export const ButtonUI = ({ to, label, urlTo , style, className, size = 'small', type = 'primary', onClick }) => {
     return to ? (
         <Link to={to}>
-            <Button type={type} size={size} style={style} onClick={onClick}>
+            <Button className={className} type={type} size={size} style={style} onClick={onClick}>
                 {label}
             </Button>
         </Link>
     ) : urlTo ? (
         <a href={urlTo}>
-            <Button type={type} size={size} style={style} onClick={onClick}>
+            <Button className={className} type={type} size={size} style={style} onClick={onClick}>
                 {label}
             </Button>
         </a>
     ) : (
-        <Button type={type} size={size} style={style} onClick={onClick}>
+        <Button className={className} type={type} size={size} style={style} onClick={onClick}>
             {label}
         </Button>
     );
