812ac89430d11b98ede12ab7bedf101254891e9d
Vlad Kost <kostvkthekost@gmail.com>
2023-03-22 00:12:50 +0300

    v2.0.0

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import multer from "multer";
+import cors from 'cors';
 
 import {PostController, UserController} from './controllers/index.js';
 import {loginValidation, postCreateValidation, registerValidation} from "./validations.js";
@@ -24,6 +25,7 @@ const storage = multer.diskStorage({
 const upload = multer({storage});
 
 app.use(express.json());
+app.use(cors());
 app.use('/media', express.static('media'));
 
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
@@ -36,7 +38,10 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
     });
 });
 
+// app.get('/tags', PostController.getLastTags);
+
 app.get('/posts', PostController.getAll);
+app.get('/posts/tags', PostController.getLastTags);
 app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.remove);
