a79e503588a3727126085ef31d97a8813dc89ba8
Vlad Kost <kostvkthekost@gmail.com>
2023-03-24 22:54:21 +0300

    v2.0.0

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import {PostController, UserController} from './controllers/index.js';
 import {loginValidation, postCreateValidation, registerValidation} from "./validations.js";
 import {checkAuth, handleValidationErrors} from './utils/index.js';
 
-mongoose.connect(MONGODB_URL,).then(() => console.log('DB ok')).catch((err) => console.log('DB error', err));
+mongoose.connect(REACT_APP_API_MONGODB_URL).then(() => console.log('DB ok')).catch((err) => console.log('DB error', err));
 
 const app = express();
 
