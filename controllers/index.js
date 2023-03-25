fd127715424602ed9b1bf4a108b05f9feee3748f
TheKost_ <thekost2@yandex.ru>
2023-03-25 23:17:38 +0300

    v2.0.0

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,3 @@
-export * as UserController from "./UserController.js";
-export * as PostController from "./PostController.js";
+export UserController from "./UserController.js";
+export PostController from "./PostController.js";
 
