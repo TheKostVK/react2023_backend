bd55c12b2b25e640b96c3d0a83b274658604c12b
TheKost_ <thekost2@yandex.ru>
2023-03-25 23:28:47 +0300

    v2.0.0

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ import express from "express";
 import mongoose from "mongoose";
 import multer from "multer";
 import cors from 'cors';
-import path from "path";
+import * as path from "path";
 
 import {PostController, UserController} from './controllers/index.js';
 import {loginValidation, postCreateValidation, registerValidation} from "./validations.js";
 import {checkAuth, handleValidationErrors} from './utils/index.js';
 
-mongoose.connect(
-    'mongodb+srv://TheKost:AD6-9PP-Vt9-n6D@cluster0.fkbk1nc.mongodb.net/blog?retryWrites=true&w=majority'
-).then(() => console.log('DB ok')).catch((err) => console.log('DB error', err));
+mongoose.connect("mongodb+srv://TheKost:AD6-9PP-Vt9-n6D@cluster0.fkbk1nc.mongodb.net/blog?retryWrites=true&w=majority").then(() => console.log('DB ok')).catch((err) => console.log('DB error', err));
+
 
 const app = express();
+
 
 // Настройки хранения загруженных файлов
 const storage = multer.diskStorage({
