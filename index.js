84d5f01f1b1fb421ec5dde6079263e1e9f3f82c5
TheKost_ <thekost2@yandex.ru>
2023-03-25 23:38:28 +0300

    v2.0.0

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ import {PostController, UserController} from './controllers/index.js';
 import {loginValidation, postCreateValidation, registerValidation} from "./validations.js";
 import {checkAuth, handleValidationErrors} from './utils/index.js';
 
-mongoose.connect("mongodb+srv://TheKost:AD6-9PP-Vt9-n6D@cluster0.fkbk1nc.mongodb.net/blog?retryWrites=true&w=majority").then(() => console.log('DB ok')).catch((err) => console.log('DB error', err));
+mongoose.connect(
+    process.env.REACT_APP_API_DB_URL
+).then(() => console.log('DB ok')).catch((err) => console.log('DB error', err));
 
 
 const app = express();
